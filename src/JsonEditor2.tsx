85674fa76a6f55a208fc9c1118ffe2b632097d94
Masaki Hara <ackie.h.gmai@gmail.com>
2023-04-11 21:56:23 +0900

    Updater for new editor

--- src/JsonEditor2.tsx
+++ src/JsonEditor2.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 
 export type JsonEditor2Props = {
   value: unknown;
   setValue: (updater: (prevValue: unknown) => unknown) => void;
 };
 
+const emptyPath: Path = [];
 export const JsonEditor2: React.FC<JsonEditor2Props> = (props) => {
   const { value, setValue } = props;
+  const setPartValue = useCallback((path: Path, updater: (prevValue: unknown) => unknown) => {
+    setValue((prevValue) => updatePath(path, prevValue, updater));
+  }, [setValue]);
+  return <JsonPartEditor path={emptyPath} value={value} setValue={setPartValue} />;
+}
+
+type Segment = string | number;
+type Path = readonly Segment[];
+type JsonPartEditorProps = {
+  path: Path;
+  value: unknown;
+  setValue: (path: Path, updater: (prevValue: unknown) => unknown) => void;
+};
+
+const JsonPartEditor: React.FC<JsonPartEditorProps> = React.memo((props) => {
+  const { path, value, setValue } = props;
 
   if (typeof value === "string") {
     return <span>{JSON.stringify(value)}</span>;
@@ -23,7 +40,7 @@ export const JsonEditor2: React.FC<JsonEditor2Props> = (props) => {
         {
           value.map((elem, i, a) => (
             <React.Fragment key={i}>
-              <JsonEditor2 value={elem} setValue={setValue} />
+              <JsonChildEditor parentPath={path} nextSegment={i} value={elem} setValue={setValue} />
               {i + 1 === a.length ? "" : ","}
             </React.Fragment>
           ))
@@ -39,7 +56,7 @@ export const JsonEditor2: React.FC<JsonEditor2Props> = (props) => {
         {
           Object.entries(record).map(([key, value], i, a) => (
             <React.Fragment key={key}>
-              {JSON.stringify(key)}: <JsonEditor2 value={value} setValue={setValue} />
+              {JSON.stringify(key)}: <JsonChildEditor parentPath={path} nextSegment={key} value={value} setValue={setValue} />
               {i + 1 === a.length ? "" : ","}
             </React.Fragment>
           ))
@@ -49,7 +66,52 @@ export const JsonEditor2: React.FC<JsonEditor2Props> = (props) => {
     );
   }
   return null;
+});
+
+type JsonChildEditorProps = Omit<JsonPartEditorProps, "path"> & {
+  parentPath: Path,
+  nextSegment: Segment,
 };
+
+const JsonChildEditor: React.FC<JsonChildEditorProps> = (props) => {
+  const { parentPath, nextSegment, ...rest } = props;
+  const path = useMemo(() => [...parentPath, nextSegment], [parentPath, nextSegment]);
+  return <JsonPartEditor path={path} {...rest} />
+};
+
+function updatePath(path: Path, prevValue: unknown, updater: (prevValue: unknown) => unknown): unknown {
+  const stack: ([any[], number] | [Record<string, unknown>, string])[] = [];
+  let current = prevValue;
+  for (const key of path) {
+    if (Array.isArray(current) && typeof key === "number") {
+      stack.push([current, key]);
+      current = current[key];
+    } else if (
+      typeof current === "object"
+      && current != null
+      && !Array.isArray(current)
+      && typeof key === "string"
+    ) {
+      stack.push([asRecord(current), key]);
+      current = asRecord(current)[key];
+    } else {
+      // Give up updating
+      return prevValue;
+    }
+  }
+  current = updater(current);
+  while (stack.length > 0) {
+    const [prevPart, key] = stack.pop()!;
+    if (typeof key === "number") {
+      const newArray = [...prevPart as any[]];
+      newArray[key] = current;
+      current = newArray;
+    } else {
+      current = { ...prevPart, key: current };
+    }
+  }
+  return current;
+}
 
 function asRecord(obj: object): Record<string, unknown> {
   return obj as Record<string, unknown>;
